e33b66371c7ccd3f0569687170983bd8f2723267
alexander.schaal <alexander.schaal@esrlabs.com>
2015-01-05 09:20:56 +0100

    Bakery uses relative paths for -m

--- lib/common/ext/file.rb
+++ lib/common/ext/file.rb
@@ -13,53 +13,53 @@ class File
 
     return nil if from.nil? or to.nil?
     
-	toSplitted = to.split('/')
-	fromSplitted = from.split('/')
-	
-	max = [toSplitted.length, fromSplitted.length].min
-
-	
-	return nil if max < 1
-	
-	i = 0
-
-	# path letter in windows may be case different
-  toIsWindowsAbs = false
-	if toSplitted[0].length > 1 and fromSplitted[0].length > 1
-    toIsWindowsAbs = toSplitted[0][1] == ':'
-    i = 1  if toIsWindowsAbs and fromSplitted[0][1] == ':' and toSplitted[0][0].downcase == fromSplitted[0][0].downcase
-	end
-	
-  if (toIsWindowsAbs and i==0)
-    res = to
+    toSplitted = to.split('/')
+    fromSplitted = from.split('/')
+    
+    max = [toSplitted.length, fromSplitted.length].min
+    
+    
+    return nil if max < 1
+    
+    i = 0
+    
+    # path letter in windows may be case different
+    toIsWindowsAbs = false
+    if toSplitted[0].length > 1 and fromSplitted[0].length > 1
+      toIsWindowsAbs = toSplitted[0][1] == ':'
+      i = 1  if toIsWindowsAbs and fromSplitted[0][1] == ':' and toSplitted[0][0].downcase == fromSplitted[0][0].downcase
+    end
+    
+    if (toIsWindowsAbs and i==0)
+      res = to
+      res += "/" if endWithSlash 
+      return res	  
+    end
+    
+    while i < max
+        break if toSplitted[i] != fromSplitted[i] 
+      i += 1
+    end
+    j = i
+    
+    res = []
+    while i < fromSplitted.length
+      res << ".."
+      i += 1
+    end
+    
+    while j < toSplitted.length
+      res << toSplitted[j]
+      j += 1
+    end
+    
+    if res.length == 0
+      return ""
+    end
+    
+    res = res.join('/')
     res += "/" if endWithSlash 
-    return res	  
-  end
-	
-  while i < max
-      break if toSplitted[i] != fromSplitted[i] 
-	  i += 1
-	end
-	j = i
-	
-	res = []
-	while i < fromSplitted.length
-	  res << ".."
-	  i += 1
-	end
-	
-	while j < toSplitted.length
-	  res << toSplitted[j]
-	  j += 1
-	end
-	
-	if res.length == 0
-	  return ""
-	end
-	
-	res = res.join('/')
-	res += "/" if endWithSlash 
-	res
+    res
   end
 
   
