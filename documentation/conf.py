135a6ba11b063b23e809401d0f69340142293e7f
alexander.schaal <alexander.schaal@esrlabs.com>
2015-10-27 16:48:44 +0100

    Added new logo to docu

--- documentation/conf.py
+++ documentation/conf.py
@@ -126,7 +126,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # documentation.
 html_theme_options = {
     #'globaltoc_depth': 2,
-    'navbar_title': "bake",
+    'navbar_title': " ",
     # Tab name for entire site. (Default: "Site")
     'navbar_site_name': "Section",
     'navbar_pagenav': True,
@@ -152,12 +152,12 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/logo_conformant_48.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "_static/logo_tiny_32.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- documentation/fixBootstrap.sh
+++ documentation/fixBootstrap.sh
@@ -2,3 +2,6 @@
 search="@import url(\"\/\/fonts.googleapis.com\/css?family=Roboto:400,500\");"
 replace="@import url(\"..\/..\/roboto.css\\\");"
 sed -i "s/${search}/${replace}/g" _build/html/_static/bootswatch-3.3.4/sandstone/bootstrap.min.css
+search="navbar-brand{float:left;padding:20px 15px"
+replace="navbar-brand{float:left;padding:8px 15px"
+sed -i "s/${search}/${replace}/g" _build/html/_static/bootswatch-3.3.4/sandstone/bootstrap.min.css
