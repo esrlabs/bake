6aeef0fe28c23249fbe72dbc1340eb785064d1d8
alexander.schaal <alexander.schaal@esrlabs.com>
2016-03-14 17:28:16 +0100

    exception on exception fixed, introduced with CI setup

--- lib/common/exit_helper.rb
+++ lib/common/exit_helper.rb
@@ -27,7 +27,11 @@ at_exit do
   if Bake::ExitHelper.exit_code != 0
     exit(Bake::ExitHelper.exit_code)
   elsif not $!.nil?
-    exit($!.success? ? 0 : 1)
+    if $!.respond_to?("success")
+      exit($!.success? ? 0 : 1)
+    else
+      exit(1)
+    end
   else
     exit(0)
   end
