332f1abcaf2b5cf2eed73173525ebd694c43f8b5
alexander.schaal <alexander.schaal@esrlabs.com>
2016-05-04 11:30:46 +0200

    fixed exit code in exit handler

--- lib/common/exit_helper.rb
+++ lib/common/exit_helper.rb
@@ -6,14 +6,14 @@ module Bake
     def self.set_exit_code(val)
       @@exit_code = val
     end
-    
+
     def self.exit_code()
       @@exit_code
     end
 
     def self.reset_exit_code()
       @@exit_code = 0
-    end    
+    end
 
     def self.exit(val)
       @@exit_code = val
@@ -27,7 +27,7 @@ at_exit do
   if Bake::ExitHelper.exit_code != 0
     exit(Bake::ExitHelper.exit_code)
   elsif not $!.nil?
-    if $!.respond_to?("success")
+    if $!.respond_to?("success?")
       exit($!.success? ? 0 : 1)
     else
       exit(1)
