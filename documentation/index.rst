e91b12a28a8b4fc5c97b193638ff43d5085d03c1
alexander.schaal <alexander.schaal@esrlabs.com>
2017-02-22 14:42:04 +0100

    install doc fix

--- documentation/index.rst
+++ documentation/index.rst
@@ -1,4 +1,4 @@
-bake 2.31.2
+bake 2.31.3
 ==========================================
 bake, building software **fast** and **easy**!
 
--- lib/common/version.rb
+++ lib/common/version.rb
@@ -1,7 +1,7 @@
 module Bake
   class Version
     def self.number
-      "2.31.2"
+      "2.31.3"
     end
 
     def self.printBakeVersion(ry = "")
